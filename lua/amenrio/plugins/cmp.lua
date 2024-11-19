1a0ea2887d21cb4a5bc7a47ee3918fb6b658ab29
Andres Mendez <amenrio@gmail.com>
2024-11-19 13:15:40 +0100

    cmp: fix: fix completion snippet configuration

--- lua/amenrio/plugins/cmp.lua
+++ lua/amenrio/plugins/cmp.lua
@@ -2,24 +2,27 @@ return {
   { "windwp/nvim-autopairs", event = "InsertEnter", config = true },
   {
     "L3MON4D3/LuaSnip",
-    build = "make install_jsregexp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lua",
+      "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
   },
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
-      "neovim/nvim-lspconfig",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "L3MON4D3/LuaSnip",
       "SergioRibera/cmp-dotenv",
       "onsails/lspkind.nvim",
     },
     config = function()
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
       local cmp = require("cmp")
       local lspkind = require("lspkind")
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
       cmp.setup({
         -- Add source name to cmp menu
         formatting = {
@@ -36,15 +39,14 @@ return {
           }),
         },
         snippet = {
-          -- REQUIRED - you must specify a snippet engine
           expand = function(args)
-            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+            require("luasnip").lsp_expand(args.body)
           end,
         },
-        -- window = {
-        --   completion = cmp.config.window.bordered(),
-        --   documentation = cmp.config.window.bordered(),
-        -- },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
@@ -52,15 +54,14 @@ return {
           ["<C-e>"] = cmp.mapping.abort(),
           ["<C-y>"] = cmp.mapping.confirm({ select = true }),
         }),
-        sources = cmp.config.sources({
+        sources = {
+          { name = "lazydev" },
           { name = "nvim_lsp" },
           { name = "nvim_lua" },
-          { name = "luasnip" },
-          { name = "path" },
-        }, {
-          { name = "buffer" },
           { name = "dotenv" },
-        }),
+          { name = "luasnip" },
+          { name = "buffer" },
+        }
       })
       cmp.setup.cmdline({ "/", "?" }, {
         mapping = cmp.mapping.preset.cmdline(),
@@ -77,18 +78,8 @@ return {
         }),
         matching = { disallow_symbol_nonprefix_matching = false },
       })
-
       cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-
-      require("luasnip.loaders.from_vscode").lazy_load()
-      -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-      -- require('lspconfig')['pyright'].setup {
-      --   capabilities = capabilities
-      -- }
-      -- require('lspconfig')['lua_ls'].setup {
-      --   capabilities = capabilities
-      -- }
-    end,
+    end
   },
+
 }
